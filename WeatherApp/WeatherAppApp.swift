58997ff38b27de672e92e8c65e73f10516e1b1c9
VadimBliz <vadimbliz98@gmail.com>
2021-12-28 11:50:08 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Вадим Близнюк on 28.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
